acf7c8eaf16ab69d1cf745dff85b8419285ab53b
Sedinam29 <135133325+Sedinam29@users.noreply.github.com>
2024-06-27 07:00:23 -0500

    Add files via upload

---
+++ boot.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+read -p "Would you like to play rock paper scissors? (yes/no) " answer
+if [ "{$answer} == "yes" ]
+then
+    python3 rps_game_proj.py
+else
+    echo "That's too bad, maybe next time"
+fi
+
+
